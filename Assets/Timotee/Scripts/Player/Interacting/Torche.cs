1e14991b8e55fca40c9451faa0351468ec8b0a34
Timotee BABOULIN <t.baboulin@ensicar.lan>
2023-04-13 10:38:43 +0200

    Fixed Anims

---
+++ Assets/Timotee/Scripts/Player/Interacting/Torche.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+public class Torche : MonoBehaviour, IInteractable
+{
+    public Item Lighter;
+
+    public Light SpotLight;
+    public VisualEffect Flames;
+
+    public GameObject GameObject => gameObject;
+    public bool Interact(IActor user)
+    {
+        if (!user.Inventory.ContainItem(Lighter)) return false;
+        
+        Flames.gameObject.SetActive(true);
+        SpotLight.gameObject.SetActive(true);
+        return true;
+    }
+}
